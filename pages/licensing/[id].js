0902b998197ddbdd3863e57e1cb9dacf30e2940e
seba gaete <sebagaetevera@gmail.com>
2020-11-25 17:12:04 -0300

    asdasd

--- pages/licensing/[id].js
+++ pages/licensing/[id].js
@@ -1,11 +1,13 @@
 import {useRouter} from 'next/router';
+import licencias from './licencias.json';
 
 const Licen = ()=>{
     const router = useRouter();
-
+    const l = licencias.filter((e)=> e.name == router.query.id)[0];
+    console.log(l)
     return(
         <div>
-            {router.query.id}
+            {l.des}
         </div>
     )
 };
